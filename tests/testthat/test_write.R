a2f99e3203e03800568315fbc5d61b81b845465d
dleutnant <leutnant@fh-muenster.de>
2017-06-06 16:49:51 +0200

    adding influx_write tests

---
+++ tests/testthat/test_write.R
@@ -0,0 +1,55 @@
+
+testthat::context("testing influx_write")
+
+# setup influx connection
+
+testthat::test_that("connection", {
+  
+  # only local tests
+  testthat::skip_on_cran()
+  testthat::skip_on_travis()
+  
+  con <<- influx_connection(group = "admin")
+  
+  testthat::expect_is(object = con, class = "list")
+  
+})
+
+testthat::test_that("write xts with NA", { 
+  
+  # only local tests
+  testthat::skip_on_cran()
+  testthat::skip_on_travis()
+  
+  # prepare tmp xts object
+  tmp <- xts::xts(x = runif(10),
+                  order.by = seq(Sys.time()-3600, by = "secs", length.out = 10))
+  # add second column
+  tmp <- cbind(tmp, tmp)
+  colnames(tmp) <- c("one", "two")
+  
+  # numeric matrix
+  tmp[8,1] <- NA
+  tmp[3,2] <- NA
+  influx_write(con = con, db = "tmp", xts = tmp, measurement = "test_num")
+  influx_query(con = con, db = "tmp", query = "SELECT * from test_num")
+  
+  # integer matrix
+  tmp[,1] <- 1:10
+  tmp[,2] <- 20:21
+  tmp[8,1] <- NA
+  tmp[3,2] <- NA
+  influx_write(con = con, db = "tmp", xts = tmp, measurement = "test_int", use_integers = T)
+  influx_query(con = con, db = "tmp", query = "SELECT * from test_int")
+  
+  # character matrix
+  tmp[,1] <- paste0("NAME", 1:10)
+  tmp[,2] <- paste0("NAME_TWO_", 1:10)
+  tmp[8,1] <- NA
+  tmp[3,2] <- NA
+  influx_write(con = con, db = "tmp", xts = tmp, measurement = "test_str")
+  influx_query(con = con, db = "tmp", query = "SELECT * from test_str")
+  
+})
+  
+  
