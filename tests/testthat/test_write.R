71660a229f6b83347cb187a2d07e835e5c6235cf
dleutnant <leutnant@fh-muenster.de>
2017-06-09 13:05:46 +0200

    add influx_write mixed data type

--- tests/testthat/test_write.R
+++ tests/testthat/test_write.R
@@ -50,6 +50,19 @@ testthat::test_that("write xts with NA", {
   influx_write(con = con, db = "tmp", xts = tmp, measurement = "test_str")
   influx_query(con = con, db = "tmp", query = "SELECT * from test_str")
   
+  # mixed data.frame
+  tmp1 <- xts::xts(x = 1:1000, 
+                   order.by =  seq(as.POSIXct("1970-1-1"), by = "hours", length.out = 1000))
+  tmp2 <- xts::xts(x = paste0("NAME_TWO_", 1:1000),
+                   order.by =  seq(as.POSIXct("1970-1-1"), by = "hours", length.out = 1000))
+  tmp1[8] <- NA
+  tmp2[3] <- NA
+  colnames(tmp1) <- c("one")
+  colnames(tmp2) <- c("two")
+  influx_write(con = con, db = "tmp", xts = tmp1, measurement = "test_df")
+  influx_write(con = con, db = "tmp", xts = tmp2, measurement = "test_df")
+  influx_query(con = con, db = "tmp", query = "SELECT * from test_df limit 1000")
+  
 })
   
   
